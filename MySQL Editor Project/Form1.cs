aea8eacc4fe454d5809ebbbe2cf4b812fe27f726
devkyj <program.kyj@gmail.com>
2024-04-08 17:28:01 +0900

    초기 커밋.

---
+++ MySQL Editor Project/Form1.cs
@@ -0,0 +1,20 @@
+namespace MySQL_Editor_Project
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
